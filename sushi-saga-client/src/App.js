f0beefeb3b511c24d1fcb4544d76a7debdc9e915
aibek <macbook@Macbooks-MacBook-Pro.local>
2019-12-02 09:46:12 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,14 +3,32 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
+
+
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+state = {
+  sushis : []
+}
+
+
+  componentDidMount() {
+    fetch(API)
+    .then(r => r.json())
+    .then(data=> {
+      this.setState({
+        sushis: data
+      })
+    })
+  }
+
   render() {
+
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer sushis={this.state.sushis} />
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,14 +1,22 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
+        
+            {props.sushis.map(sushi=> {
+            return <Sushi key={sushi.id} sushi={sushi} />     
+            })
+          //   "id": 1,
+          //   "name": "Tako Supreme",
+          //   "img_url": "https://sushistickers.com/img/sushi-slice_99.png",
+          //   "price": 20,
+          //   "created_at": "2018-07-27T18:53:16.241Z"
+          // },
+            
         }
         <MoreButton />
       </div>
